82b47c6f305f995fd926baf55e4c3a48cb7c84eb
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2022-06-19 14:00:54 +0200

    Fixed endpoints

--- src/main/java/com/example/masimpl/Repositories/CarDamageRepository.java
+++ src/main/java/com/example/masimpl/Repositories/CarDamageRepository.java
@@ -2,7 +2,9 @@ package com.example.masimpl.Repositories;
 
 import com.example.masimpl.Models.CarDamage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource //   http://localhost:8080/carDamagescarDamages
 public interface CarDamageRepository extends JpaRepository<CarDamage, Long> {
 
 }
--- src/main/java/com/example/masimpl/Repositories/TechnicalServiceRepository.java
+++ src/main/java/com/example/masimpl/Repositories/TechnicalServiceRepository.java
@@ -4,7 +4,7 @@ import com.example.masimpl.Models.TechnicalService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource // http://localhost:8080/technicalServices/
 public interface TechnicalServiceRepository extends JpaRepository<TechnicalService, Long> {
 
 }
